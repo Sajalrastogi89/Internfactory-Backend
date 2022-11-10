f83a85008b8a6dfd4f9cdf036f509252ddf77122
Pranav Bisaria <pranavbisariya29@gmail.com>
2022-11-11 00:23:34 +0530

    Added pagination on searchUserByInternship

--- src/main/java/com/OurInternfactory/Repositories/UserRepo.java
+++ src/main/java/com/OurInternfactory/Repositories/UserRepo.java
@@ -12,5 +12,5 @@ import java.util.Optional;
 @Repository
 public interface UserRepo extends JpaRepository<User, Integer>{
     Optional<User> findByEmail(String email);
-
+    Page<User> findByInterships(Internships internships, Pageable pageable);
 }
