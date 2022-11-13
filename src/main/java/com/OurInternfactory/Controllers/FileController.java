4f7e4bceff1b502b83661b6c826ef9ab0132df53
Pranav Bisaria <pranavbisariya29@gmail.com>
2022-11-14 02:45:19 +0530

    Renoved security from file serving

--- src/main/java/com/OurInternfactory/Controllers/FileController.java
+++ src/main/java/com/OurInternfactory/Controllers/FileController.java
@@ -4,31 +4,27 @@ import com.OurInternfactory.Payloads.FileDto;
 import com.OurInternfactory.Services.FileServices;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.InputStream;
 
 @RestController
 @RequestMapping("/file")
 public class FileController {
-
     private final FileServices fileServices;
     @Value("${project.image}")
     private String path;
-
-    public FileController(FileServices fileServices) throws IOException {
+    public FileController(FileServices fileServices){
         this.fileServices = fileServices;
     }
 
+
+    //Upload FILE API
     @PostMapping("/upload")
     public ResponseEntity<FileDto> fileUpload(
-        @RequestParam("image") MultipartFile image
+            @RequestParam("image") MultipartFile image
     ){
         String filename = null;
         try {
@@ -41,11 +37,14 @@ public class FileController {
         return new ResponseEntity<>(new FileDto(filename, "Image is Successfully Uploaded !!!"), HttpStatus.OK);
     }
 
-    @GetMapping(value  = "/images/{imageName}", produces = MediaType.IMAGE_JPEG_VALUE)
-    public void downloadImage(@PathVariable("imageName") String imageName, HttpServletResponse response) throws IOException {
-        InputStream resource = this.fileServices.getImage(path, imageName);
-        response.setContentType(MediaType.IMAGE_JPEG_VALUE);
-        StreamUtils.copy(resource, response.getOutputStream());
-    }
+
+    //Serving API not to be used
+
+//    @GetMapping(value  = "/images/{imageName}", produces = MediaType.IMAGE_JPEG_VALUE)
+//    public void downloadImage(@PathVariable("imageName") String imageName, HttpServletResponse response) throws IOException {
+//        InputStream resource = this.fileServices.getImage(path, imageName);
+//        response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+//        StreamUtils.copy(resource, response.getOutputStream());
+//    }
     // localhost:8080/files/abc.png
 }
