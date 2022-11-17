0abbb214b7060c2c9b50358be37ec35d283598ae
Pranav Bisaria <pranavbisariya29@gmail.com>
2022-11-18 00:03:41 +0530

    Added Submission Model Dto

---
+++ src/main/java/com/OurInternfactory/Payloads/SubmissionModelDto.java
@@ -0,0 +1,17 @@
+package com.OurInternfactory.Payloads;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SubmissionModelDto {
+    private Integer id;
+    private List<Question> questions;
+}
