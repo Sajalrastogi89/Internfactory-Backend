b03df75da5b10b2031e2be23fd3b4a023b466ee1
Sajal Rastogi <101357667+Sajalrastogi89@users.noreply.github.com>
2022-11-19 07:58:20 +0530

    Added export function implementation

--- src/main/java/com/OurInternfactory/Services/Impl/UserServiceImpl.java
+++ src/main/java/com/OurInternfactory/Services/Impl/UserServiceImpl.java
@@ -179,6 +179,148 @@ public class UserServiceImpl implements UserService {
         return cat.stream().map(this::InternshipToDto).collect(Collectors.toList());
 
     }
+    
+        @Override
+    public void export(HttpServletResponse response, CVGenerator cvData) throws IOException {
+        System.out.println(cvData.getName()+cvData.getSkill1());
+        Document document;
+        document = new Document(PageSize.A4);
+        PdfWriter.getInstance(document, response.getOutputStream());
+
+        document.open();
+        Font fontTitle= FontFactory.getFont(FontFactory.HELVETICA_BOLD);
+        fontTitle.setSize(18);
+
+        Paragraph paragraph=new Paragraph(cvData.getName()+"\n",fontTitle);
+        paragraph.setAlignment(Paragraph.ALIGN_CENTER);
+
+        Font fontParagraph=FontFactory.getFont(FontFactory.HELVETICA);
+        fontParagraph.setSize(15);
+        Font fontParagraph1=FontFactory.getFont(FontFactory.HELVETICA);
+        fontParagraph1.setSize(10);
+        Font fontParagraph2=FontFactory.getFont(FontFactory.HELVETICA);
+        fontParagraph2.setSize(12);
+//Skills
+        Paragraph paragraph2=new Paragraph("SKILLS\n",fontParagraph);
+        paragraph2.setAlignment(Paragraph.ALIGN_LEFT);
+        Paragraph paragraph3=new Paragraph(cvData.getSkill1()+"\n"+cvData.getSkill2()+"\n"+cvData.getSkill3()+"\n"+cvData.getSkill4()+"\n"+cvData.getSkill5()+"\n",fontParagraph1);
+        paragraph3.setAlignment(Paragraph.ALIGN_LEFT);
+
+        //Projects
+
+        Paragraph paragraph4=new Paragraph("PROJECTS\n",fontParagraph);
+        paragraph4.setAlignment(Paragraph.ALIGN_LEFT);
+//Project1
+        Paragraph paragraph5=new Paragraph(cvData.getProject1Heading(),fontParagraph2);
+        paragraph5.setAlignment(Paragraph.ALIGN_LEFT);
+
+        Paragraph paragraph6=new Paragraph(cvData.getProject1Description(),fontParagraph1);
+        paragraph6.setAlignment(Paragraph.ALIGN_LEFT);
+        //Project2
+
+
+        Paragraph paragraph8=new Paragraph(cvData.getProject2Heading(),fontParagraph2);
+        paragraph8.setAlignment(Paragraph.ALIGN_LEFT);
+
+        Paragraph paragraph9=new Paragraph(cvData.getProject2Description(),fontParagraph1);
+        paragraph9.setAlignment(Paragraph.ALIGN_LEFT);
+
+
+        //Project3
+        Paragraph paragraph10=new Paragraph(cvData.getProject3Heading(),fontParagraph2);
+        paragraph10.setAlignment(Paragraph.ALIGN_LEFT);
+
+        Paragraph paragraph11=new Paragraph(cvData.getProject3Description(),fontParagraph1);
+        paragraph11.setAlignment(Paragraph.ALIGN_LEFT);
+
+
+        //Experiences
+        Paragraph paragraph80=new Paragraph("EXPERIENCE\n",fontParagraph);
+        paragraph80.setAlignment(Paragraph.ALIGN_LEFT);
+        //Experience1
+        Paragraph paragraph12=new Paragraph(cvData.getExperience1(),fontParagraph2);
+        paragraph12.setAlignment(Paragraph.ALIGN_LEFT);
+
+        Paragraph paragraph13=new Paragraph(cvData.getExperience1role(),fontParagraph1);
+        paragraph13.setAlignment(Paragraph.ALIGN_LEFT);
+
+        //Experience2
+        Paragraph paragraph14=new Paragraph(cvData.getExperience2(),fontParagraph2);
+        paragraph14.setAlignment(Paragraph.ALIGN_LEFT);
+
+        Paragraph paragraph15=new Paragraph(cvData.getExperience2role(),fontParagraph1);
+        paragraph15.setAlignment(Paragraph.ALIGN_LEFT);
+        //Experience3
+        Paragraph paragraph16=new Paragraph(cvData.getExperience3(),fontParagraph2);
+        paragraph16.setAlignment(Paragraph.ALIGN_LEFT);
+
+        Paragraph paragraph17=new Paragraph(cvData.getExperience3role(),fontParagraph1);
+        paragraph17.setAlignment(Paragraph.ALIGN_LEFT);
+
+
+
+        //Awards
+        Paragraph paragraph90=new Paragraph("AWARDS\n",fontParagraph);
+        paragraph90.setAlignment(Paragraph.ALIGN_LEFT);
+        //Award1
+        Paragraph paragraph18=new Paragraph(cvData.getAward1(),fontParagraph1);
+        paragraph18.setAlignment(Paragraph.ALIGN_LEFT);
+
+        //Award2
+        Paragraph paragraph19=new Paragraph(cvData.getAward2(),fontParagraph1);
+        paragraph19.setAlignment(Paragraph.ALIGN_LEFT);
+        //Award3
+        Paragraph paragraph20=new Paragraph(cvData.getAward3(),fontParagraph1);
+        paragraph20.setAlignment(Paragraph.ALIGN_LEFT);
+
+
+
+
+        //Marks
+        Paragraph paragraph21=new Paragraph("PERCENTAGE\n",fontParagraph);
+        paragraph21.setAlignment(Paragraph.ALIGN_LEFT);
+//University
+        Paragraph paragraph22=new Paragraph("University Percentage:"+cvData.getUniversityMarks(),fontParagraph);
+        paragraph22.setAlignment(Paragraph.ALIGN_LEFT);
+        //12th
+        Paragraph paragraph23=new Paragraph("12th Percentage:"+cvData.getTwelth_marks(),fontParagraph);
+        paragraph23.setAlignment(Paragraph.ALIGN_LEFT);
+        //10th
+        Paragraph paragraph24=new Paragraph("10th Percentage:"+cvData.getTenth_marks(),fontParagraph);
+        paragraph24.setAlignment(Paragraph.ALIGN_LEFT);
+
+        document.add(paragraph);
+        document.add(paragraph2);
+        document.add(paragraph3);
+        document.add(paragraph4);
+        document.add(paragraph5);
+        document.add(paragraph6);
+        document.add(paragraph8);
+        document.add(paragraph9);
+        document.add(paragraph10);
+        document.add(paragraph11);
+        document.add(paragraph80);
+        document.add(paragraph12);
+        document.add(paragraph13);
+        document.add(paragraph14);
+        document.add(paragraph15);
+        document.add(paragraph16);
+        document.add(paragraph17);
+        document.add(paragraph90);
+        document.add(paragraph18);
+        document.add(paragraph19);
+        document.add(paragraph20);
+        document.add(paragraph21);
+        document.add(paragraph22);
+        document.add(paragraph23);
+        document.add(paragraph24);
+
+
+        document.close();
+
+
+    }
+
 
     public User DtoToUser(UserDto userdto) {
         return this.modelMapper.map(userdto, User.class);
